133901bb73ac0aa219561d17f10dd9947693e1fd
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2023-03-03 23:18:54 +0100

    TestMethodOrder(MethodOrderer.Random.class)

--- src/test/java/com/foureverhh/calculator/DemoRepeatedTest.java
+++ src/test/java/com/foureverhh/calculator/DemoRepeatedTest.java
@@ -1,6 +1,8 @@
 package com.foureverhh.calculator;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -14,7 +16,9 @@ public class DemoRepeatedTest {
     }
     @RepeatedTest(value = 3, name = "{displayName}, Repetition {currentRepetition} of {totalRepetitions}")
     @DisplayName("Division by Zero")
-    void disable_testIntegerDivision_WhenDividendIsDividedByTwo_ShouldThrowArithmeticException(RepetitionInfo repetitionInfo, TestInfo testInfo) {
+    void disable_testIntegerDivision_WhenDividendIsDividedByTwo_ShouldThrowArithmeticException(
+            RepetitionInfo repetitionInfo,
+            TestInfo testInfo) {
         System.out.println("Running " + testInfo.getTestMethod().get().getName());
         System.out.println("Repetition #" + repetitionInfo.getCurrentRepetition() + " of " + repetitionInfo.getTotalRepetitions());
         // calculator = new Calculator();  //  replace by @BeforeEach
